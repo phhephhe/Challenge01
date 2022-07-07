ac55d6f95cad5f43ff460dd59a82f1595265a405
phhephhe <beridzedavit07@gmail.com>
2022-07-08 03:56:45 +0400

    added X icon

--- js.js
+++ js.js
@@ -1,8 +1,12 @@
 let login = document.querySelector('.login-photo');
 let form = document.querySelector('.form');
-
+let icon = document.querySelector('.icon');
 
 login.addEventListener('click', (event) =>{
     login.classList.add('display-none');
     form.classList.add('display-block');
 })
+
+icon.addEventListener('click', (event) => {
+    form.classList.remove('display-block');
+}) 
